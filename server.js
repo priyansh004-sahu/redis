959ca57571a83c0f87cb4375884d54fad8a4c9c3
priyansh sahu <sahupriyansh298@gmail.com>
2025-07-21 22:00:05 +0530

    .

---
+++ server.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const server = net.createServer(connection => {
+    console.log("client connected...");
+    
+    connection.on('data', data=>{
+        console.log('=>', data.toString());
+        
+    })
+})
+
+server.listen(8000, ()=>{
+    console.log("Custom Redis server running on port 8000");
+    
+})
